a33bdc1b61bab1adb8e4505ff96eae68970ad461
ArtemSerdechnyi <artem.serdechnyy@gmail.com>
2023-12-08 01:01:28 +0200

    created models "Tag", "Problems", "Comment" in problems app

--- mathinker/problems/models.py
+++ mathinker/problems/models.py
@@ -1,3 +1,60 @@
+from django.contrib.auth.models import User  # todo mb create custom user model?
 from django.db import models
 
-# Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Problem(models.Model):
+    class DifficultyChoices(models.IntegerChoices):
+        EASY = 1
+        MEDIUM = 2
+        HARD = 3
+
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    tags = models.ManyToManyField(Tag, related_name="problems")
+    difficulty = models.CharField(max_length=1, choices=DifficultyChoices)
+    created_by = models.ForeignKey(User,
+                                   on_delete=models.PROTECT,
+                                   related_name="problems")
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User,
+                                   on_delete=models.CASCADE,
+                                   related_name="like")
+    dislike = models.ManyToManyField(User,
+                                     on_delete=models.CASCADE,
+                                     related_name="dislike")
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ["difficulty", ]
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User,
+                             on_delete=models.CASCADE,
+                             related_name="comments")
+    problem = models.ForeignKey(Problem,
+                                on_delete=models.CASCADE,
+                                related_name="comments")
+    text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    likes = models.ManyToManyField(User,
+                                   on_delete=models.CASCADE,
+                                   related_name="like")
+    dislike = models.ManyToManyField(User,
+                                     on_delete=models.CASCADE,
+                                     related_name="dislike")
+
+    def __str__(self):
+        return f"{self.user} - {self.problem}"
+
+    class Meta:
+        ordering = ["created_at", ]
