fbef33bef40c9267068b1ed6901097042aa239ce
ArtemSerdechnyi <artem.serdechnyy@gmail.com>
2023-12-09 22:21:31 +0200

    register models in admin for problems app

--- mathinker/problems/admin.py
+++ mathinker/problems/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import (
+    Tag,
+    Theme,
+    Problem,
+    Comment,
+)
+
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+
+@admin.register(Theme)
+class ThemeAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+
+@admin.register(Problem)
+class ProblemAdmin(admin.ModelAdmin):
+    show_full_result_count = False
+
+    prepopulated_fields = {"slug": ("title",)}
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    show_full_result_count = False
